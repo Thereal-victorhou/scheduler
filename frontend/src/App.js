e6b49f9ba87460cdd5171b0e0c67d0cf12891691
Victor Hou <hou_v@yahoo.com>
2025-08-19 21:23:37 -0700

    modified frontend app.js index.js and store.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTodos, addTodo } from './actions/todoActions';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const dispatch = useDispatch();
+	const todos = useSelector((state) => state.todos.todos);
+
+	useEffect(() => {
+		dispatch(fetchTodos());
+	}, [dispatch]);
+
+	const handleAdd = () => {
+		dispatch(addTodo({ text: 'New Todo' }));
+	};
+
+	return (
+		<div>
+			<h1>TODO List</h1>
+			<button onClick={handleAdd}>Add Todo</button>
+			<ul>
+				{todos.map((todo) => (
+					<li key={todo.id}>{todo.text}</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
